497779f6d6be653c267332789bc02511c970b607
Robert Miner <RobertRMiner@gmail.com>
2014-12-19 14:59:11 -0600

    Add devise and modify and use devise views (for now)

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(
+  [
+    { first_name: "Robert", last_name: "Miner", email: "robert@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Emily", last_name: "Miner", email: "emily@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Greg", last_name: "Chyson", email: "greg@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Jason", last_name: "Chyson", email: "jason@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Tom", last_name: "Stump", email: "tom@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Christian", last_name: "Romero", email: "christian@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Sam", last_name: "Akers", email: "sam@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Max", last_name: "Hart", email: "max@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Charlotte", last_name: "Miner", email: "Charlotte@dreadball.com", password: "password", password_confirmation: "password" },
+    { first_name: "Roland", last_name: "Miner", email: "Roland@dreadball.com", password: "password", password_confirmation: "password" }
+  ]
+)
